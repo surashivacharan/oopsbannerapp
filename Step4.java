a6e7b0546444dbd4f55d0a52872ca7fdd6df87b6
HelloApp <shiva720710@gmail.com.in>
2026-03-02 00:37:37 +0530

    UC4-added

--- Step4.java
+++ Step4.java
@@ -1,22 +1,17 @@
-public class Step4 {
-
+public class step4 {
     public static void main(String[] args) {
 
-        
-        String[] lines = {
+        String[] bannerLines = new String[7];
 
-            String.join("  ", " ***** ", " ***** ", " ***** ", " ***** "),
-            String.join("  ", "*     *", "*     *", "*     *", "*      "),
-            String.join("  ", "*     *", "*     *", "*     *", "*      "),
-            String.join("  ", "*     *", "*     *", " ***** ", " ***** "),
-            String.join("  ", "*     *", "*     * ","*      ", "      *"),
-            String.join("  ", "*     *", "*     * ","*      ", "      *"),
-            String.join("  ", " ***** ", " *****  ","*      ",  "******")
+        bannerLines[0] = String.join("", "  *       *      ", "**      ", "**");
+        bannerLines[1] = String.join("", " *   *     *   *     ", "*   *    ", "*");
+        bannerLines[2] = String.join("", "*     *   *     *    ", "**      ", "**");
+        bannerLines[3] = String.join("", "*     *   *     *    ", "*       ", "*");
+        bannerLines[4] = String.join("", "*     *   *     *    ", "*       ", "*");
+        bannerLines[5] = String.join("", " *   *     *   *     ", "*       ", "*   *");
+        bannerLines[6] = String.join("", "  *       *      ", "*       ", "*");
 
-        };
-
-        
-        for (String line : lines) {
+        for (String line : bannerLines) {
             System.out.println(line);
         }
     }
